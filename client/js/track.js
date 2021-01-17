1367f5d98b013dd94e65fc731bd3f321b39c4428
Abhilash Vedwan <abhi211193@gmail.com>
2021-01-17 06:44:24 +0530

    <Abhilash 1/17/2021> Updated tracker to send separate parallel requests.

--- client/js/track.js
+++ client/js/track.js
@@ -1,6 +1,6 @@
 import {account, prices, update} from './api';
 import {itemSearch} from './search';
-import {asDoller, asElement, asSearchItem, findAncestor, randomColor} from './util';
+import {asDoller, asElement, asSearchItem, findAncestor} from './util';
 
 import trackTemplate from '../template/track.html';
 
@@ -32,34 +32,29 @@ function notify(id, name, price) {
 function trackPrices(container) {
     if (!items.size) return;
 
-    const max = values => values.filter(a => a).reduce((a, b) => Math.max(a, b), -1);
-    const chartData = data => {
-        const maxValue = max(data), minValue = data.filter(a => a).reduce((a, b) => Math.min(a, b), maxValue);
-        const expected = maxValue + minValue;
-        return data.map(a => a && (a * 100 / expected).toFixed(0) || '_');
-    };
     const values = Array.from(items);
-    prices(values.map(([, id]) => id), 1).then(a => a.map(([[b]], i) => [values[i][0], b])).then(prices => {
-        container.innerHTML = '';
+    Promise.all(values.map(([, id]) => prices([id], 1))).then(a => a.map(([[[b]]], i) => [values[i][0], b])).then(prices => {
         const priceMap = new Map(prices);
+        const format = a => Number(a.toFixed(2));
 
         values.forEach(([name]) => {
-            const {color, values: data} = history.get(name);
+            const {values} = history.get(name);
             const value = priceMap.get(name);
-            data.shift();
-            data.push(value);
+            values.shift();
+            values.push(value);
 
-            const image = 'https://image-charts.com/chart?cht=ls&chs=600x100&chm=B,' + color.slice(1) +
-                ',0,0,0&chxr=0,1,100&chd=t:' + chartData(data).join(',');
+            const max = values.filter(a => a).reduce((a, b) => Math.max(a, b), -1);
+            const min = values.filter(a => a).reduce((a, b) => Math.min(a, b), max);
+            const data = values.map(a => a ? format((a - min) * 100 / max) : '_');
+            const limit = format(100 - min * 100 / max);
+
+            const image = `https://image-charts.com/chart?cht=ls&chs=600x100&chm=B,72BD60,0,0,0&chxr=0,0,${limit}&chd=t:${data.join(',')}`;
             const style = 'background: url(' + image + ') no-repeat; background-size: 100% 100%;';
-            container.appendChild(asElement(`
-                <div data-name='${name}' class='tracker card'>
-                    <div class='card-body' style='${style}'>
-                        <strong>${name}: </strong>${asDoller(value)}
-                    </div>
-                </div>`));
+            const elem = container.querySelector(`#track-${items.get(name)} .card-body`);
+            elem.setAttribute('style', style);
+            elem.innerHTML = `<strong>${name}: </strong>${value && asDoller(value) || '...'}`;
 
-            if (constraints.some(({check}) => check(items.get(name), 1 - value / max(data)))) {
+            if (!!value && constraints.some(({check}) => check(items.get(name), 1 - value / max))) {
                 notify(items.get(name), name, asDoller(value));
             }
         });
@@ -75,11 +70,22 @@ function trackPrices(container) {
 
 function startTrack(tab, {id, name}) {
     items.set(name, id);
-    history.set(name, history.get(name) || {values: new Array(MAX_HISTORY).fill(undefined), color: randomColor()});
+    history.set(name, history.get(name) || {values: new Array(MAX_HISTORY).fill(undefined)});
+
+    const elem = asElement(`
+        <div id='track-${id}' data-name='${name}' class='card'>
+            <div class='card-body'>
+                <strong>${name}: </strong> ...
+            </div>
+        </div>`);
+    tab.querySelector('#prices').appendChild(elem);
 }
 
 function stopTrack(tab, id, name) {
     items.delete(name);
+
+    const elem = tab.querySelector('#prices #track-' + id);
+    elem.parentNode.removeChild(elem);
 }
 
 export function init(parent) {
@@ -113,12 +119,5 @@ export function init(parent) {
     });
 
     const priceContainer = trackTab.querySelector('#prices');
-    priceContainer.addEventListener('click', ({target}) => {
-        const tracker = findAncestor(target, '.tracker');
-        if (!tracker) return;
-
-        const log = history.get(tracker.dataset.name);
-        if (log) log.color = randomColor();
-    });
     setInterval(() => trackPrices(priceContainer), 15 * 1000);
 }
